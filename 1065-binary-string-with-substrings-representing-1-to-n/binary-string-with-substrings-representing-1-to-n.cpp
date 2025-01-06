9e4c37e730b8db1e39e28ffb847bb151b3f842e4
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-01-06 20:29:20 +0530

    Time: 4 ms (19.44%) | Memory: 8.8 MB (60.19%) - LeetSync

---
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.cpp
@@ -0,0 +1,19 @@
+#define ll long long int
+class Solution {
+public:
+    unordered_map<ll,bool>mp;
+    bool queryString(string s, int n) {
+        int N=s.length(),num=0;
+        mp.clear();
+        for(int i=0;i<N;i++){
+            num=0;
+            for(int j=i;j<N;j++){
+                num<<=1;
+                num|=(s[j]-'0');
+                if(num<=n&&num>=1)
+                    mp[num]=1;
+            }
+        }
+        return mp.size()==n;
+    }
+};
