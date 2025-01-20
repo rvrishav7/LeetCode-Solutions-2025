9ae213336776dc9f2f9a31489a244df5e7153001
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-01-20 21:02:12 +0530

    Time: 27 ms (39.55%) | Memory: 48.8 MB (5.50%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int fun(vector<int>&coins, int n, int amount){
+        if(amount==0)
+            return 1;
+        if(amount<0||n<0)
+            return 0;
+        if(dp[n][amount]!=-1)
+            return dp[n][amount];
+        return dp[n][amount]=fun(coins,n,amount-coins[n])+fun(coins,n-1,amount);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+5,vector<int>(amount+5,-1));
+        this->dp=dp;
+        return fun(coins,coins.size()-1,amount);
+    }
+};
