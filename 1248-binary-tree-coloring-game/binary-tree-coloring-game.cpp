42dd6655b39d265e1d1e6abd65e5023a1d69acda
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-02-09 11:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (61.65%) - LeetSync

---
+++ 1248-binary-tree-coloring-game/binary-tree-coloring-game.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode *root, int &x,int &l, int &r){
+        if(!root)
+            return 0;
+        int left=dfs(root->left,x,l,r);
+        int right=dfs(root->right,x,l,r);
+        if(root->val == x){
+            l=left;
+            r=right;
+        }
+        return left+right+1;
+    }
+    bool btreeGameWinningMove(TreeNode* root, int n, int x) {
+        int left=0,right=0,parent=0;
+        vector<int>temp(3);
+        dfs(root,x,temp[0],temp[1]);
+        temp[2]=n-(temp[0]+temp[1]+1);
+        sort(temp.begin(),temp.end());
+        return temp[2]>(temp[0]+temp[1]+1);
+    }
+};
