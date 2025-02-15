bbc4a49e779622cb4b3ee85df993dd7a31cfc530
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-02-16 01:46:37 +0530

    Time: 224 ms (57.14%) | Memory: 9.1 MB (71.18%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool fun(string &s, int sum, int i, int num){
+        if(i>=s.length()&&((sum-num)==0))
+            return true;
+        if(i>=s.length())
+            return false;
+        return fun(s,sum-num,i+1,s[i]-'0')||fun(s,sum,i+1,num*10+(s[i]-'0'));
+    }
+    int punishmentNumber(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            string s=to_string(i*i);
+            cout<<s<<endl;
+            if(fun(s,i,0,0))
+                ans+=(i*i);
+        }
+        return ans;
+    }
+};
