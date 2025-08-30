5ed0aa21998852f80bb4f08850f7236d5f0267a7
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-08-30 14:04:49 +0530

    Time: 3 ms (78.38%) | Memory: 23.8 MB (99.86%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -1,37 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void helper(TreeNode *root, string &s){
-        if(!root)
-            return;
-        s.append(to_string(root->val)); 
-        if(!root->left&&!root->right)
-            return;
-        //left sub tree
-        s.push_back('(');
-        helper(root->left,s);
-        s.push_back(')');
+    void helper(TreeNode* root, ostringstream &oss) {
+        if (!root) return;
 
-        //right sub tree
-        if(root->right){
-            s.push_back('(');
-            helper(root->right,s);
-            s.push_back(')');
+        oss << root->val;
+
+        if (!root->left && !root->right) return;
+
+        oss << "(";
+        helper(root->left, oss);
+        oss << ")";
+
+        if (root->right) {
+            oss << "(";
+            helper(root->right, oss);
+            oss << ")";
         }
     }
+
     string tree2str(TreeNode* root) {
-        string ans;
-        helper(root,ans);
-        return ans;
+        ostringstream oss;
+        helper(root, oss);
+        return oss.str();
     }
 };
