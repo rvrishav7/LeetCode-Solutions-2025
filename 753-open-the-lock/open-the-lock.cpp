fcd4f7f0b1cf4bf35033535c32e9e51a12c92228
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2025-09-07 13:39:52 +0530

    Time: 1037 ms (5.02%) | Memory: 331.6 MB (5.10%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_map<string,bool>mp,visited;
+    void inline add_queue(queue<pair<string,int>>&q, string &curr, int &d){
+        int digit,plusOne,minusOne;
+        string temp=curr;
+        for(int i=0;i<4;i++){
+            digit=curr[i]-'0';
+            plusOne=(digit+1)%10;
+            minusOne=(digit-1+10)%10;
+            temp=curr;
+            temp[i]=plusOne+'0';
+            if(!mp[temp])
+                q.push({temp,d+1});
+
+            temp=curr;
+            temp[i]=minusOne+'0';
+            if(!mp[temp])
+                q.push({temp,d+1});
+        }
+    }
+    int openLock(vector<string>& deadends, string target) {
+        int dis,ans=INT_MAX;
+        mp.clear();
+        for(auto &x: deadends)
+            mp[x]=true;
+        string curr="0000";
+        if(mp[curr])
+            return -1;
+        queue<pair<string,int>>q;
+        q.push({curr,0});
+
+        while(!q.empty()){
+            curr=q.front().first;
+            int dis=q.front().second;
+            q.pop();
+            if(curr==target)
+                ans=min(ans,dis);
+            if(visited[curr])
+                continue;
+            visited[curr]=true;
+            add_queue(q,curr,dis);
+        }
+
+        return ans==INT_MAX?-1:ans;
+    }
+};
