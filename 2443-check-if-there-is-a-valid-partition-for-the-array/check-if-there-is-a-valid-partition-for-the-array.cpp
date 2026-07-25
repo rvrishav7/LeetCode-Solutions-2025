4b0308f471508df52913387fdb5f9ea644ad1d0f
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2026-07-25 16:44:30 +0530

    Time: 1 ms (90.83%) | Memory: 91.5 MB (67.57%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,23 +1,28 @@
+short dp[100001];
 class Solution {
 public:
     int n;
     bool fun(vector<int>&nums, int n){
         if(n<0)
             return true;
+        if(dp[n]!=-1)
+            return dp[n];
         if(n==1)
-            return nums[0]==nums[1];
+            return dp[n]=(nums[0]==nums[1]);
         if(n>1){
             if(nums[n-1]==nums[n-2]&&nums[n-2]==nums[n]){   //last 3 same elements
-                return fun(nums,n-2)||fun(nums,n-3);
+                return dp[n]=fun(nums,n-2)||fun(nums,n-3);
             }
             else if(nums[n]-nums[n-1]==1 &&nums[n-1]-nums[n-2]==1)   //increasing
-                return fun(nums,n-3);
+                return dp[n]=fun(nums,n-3);
             else if(nums[n-1]==nums[n]) //last 2 same elements 
                 return fun(nums,n-2);
         }
-        return false;
+        return dp[n]=false;
     }
     bool validPartition(vector<int>& nums) {
+        for(int i=0;i<=nums.size();i++)
+            dp[i]=-1;
         return fun(nums,nums.size()-1);
     }
 };
