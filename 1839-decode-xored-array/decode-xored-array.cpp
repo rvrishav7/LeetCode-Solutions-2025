ac0506c3a9a4c18ac0c80e645b5e75509b8cf468
Rishav Kumar <33838256+rvrishav7@users.noreply.github.com>
2026-08-06 16:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (40.96%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int>ans;
+        ans.push_back(first);
+        int val=first;;
+        for(auto &x:encoded){
+            val=x^val;
+            ans.push_back(val);
+        }
+        return ans;
+    }
+};
